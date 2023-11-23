57363fd3bc562b6a72eff4501b6d171171f0808d
mjamecny <jamecny.m@tutanota.com>
2023-11-23 15:35:57 +0100

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     fontFamily: {
       sans: ["'Space Mono'", "sans-serif"],
     },
+    colors: {
+      strongCyan: "hsl(172, 67%, 45%)",
+      veryDarkCyan: "hsl(183, 100%, 15%)",
+      darkGrayishCyan: "hsl(186, 14%, 43%)",
+      grayishCyan: "hsl(184, 14%, 56%)",
+      lightGrayishCyan: "hsl(185, 41%, 84%)",
+      veryLightGrayishCyan: "hsl(189, 41%, 97%)",
+      white: "hsl(0, 0%, 100%)",
+    },
   },
   plugins: [],
 }
